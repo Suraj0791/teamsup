42d7724a92c81a10eccc4cbde1e2eaf359cb3400
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-09-11 14:00:56 +0530

    fixing inngest error

--- BACKEND/src/config/inngest.js
+++ BACKEND/src/config/inngest.js
@@ -1,11 +1,19 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import { User } from "../models/user.model.js"; // Import the User model
+import { User } from "../models/user.model.js";
 import { addUserToPublicChannels, deleteStreamUser, upsertStreamUser } from "./stream.js";
+import { ENV } from "./env.js"; // Import the ENV object
 
-// Create a client to send and receive events
-export const inngest = new Inngest({ id: "slack-clone" });
+// Create a client to send and receive events, explicitly providing the signing key
+export const inngest = new Inngest({ 
+  id: "slack-clone",
+  signingKey: ENV.INNGEST_SIGNING_KEY // This line forces the SDK to use your key
+});
 
+/**
+ * This function is triggered when a 'user.created' event is received from Clerk.
+ * It syncs the new user's data to your MongoDB and your Stream chat service.
+ */
 const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
@@ -17,33 +25,43 @@ const syncUser = inngest.createFunction(
     const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_address,
-      name: `${first_name || ""} ${last_name || ""}`,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
       image: image_url,
     };
 
+    // Create the user in your own database
     await User.create(newUser);
 
+    // Create or update the user in Stream's chat service
     await upsertStreamUser({
       id: newUser.clerkId.toString(),
       name: newUser.name,
       image: newUser.image,
     });
 
+    // Automatically add the new user to public channels
     await addUserToPublicChannels(newUser.clerkId.toString());
   }
 );
 
+/**
+ * This function is triggered when a 'user.deleted' event is received from Clerk.
+ * It removes the user's data from your MongoDB and your Stream chat service.
+ */
 const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
     await connectDB();
     const { id } = event.data;
+    
+    // Delete from your database
     await User.deleteOne({ clerkId: id });
 
+    // Delete from Stream's chat service
     await deleteStreamUser(id.toString());
   }
 );
 
-// Create an empty array where we'll export future Inngest functions
+// Export all the functions for the Inngest server to use
 export const functions = [syncUser, deleteUserFromDB];
